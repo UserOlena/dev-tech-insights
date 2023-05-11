3db1f1581abda5b252a1966673e5b304a663d06d
Olena Pashchenko <helen.watermore@gmail.com>
2023-05-11 13:53:19 -0700

    Establish a connection and set up a server to enable it to run.

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+  
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}.`);
+})
