bb5d1db1d531c2265dda4cbd00ba7d14d4ca0963
Olena Pashchenko <helen.watermore@gmail.com>
2023-05-15 15:30:27 -0700

    Establish express-session.

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 
 const express = require('express');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+}));
 
 app.use(routes);
 
